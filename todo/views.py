ac920c9d542c22c409edbeef4f0160414db29346
koko2824 <prg.koko2824@gmail.com>
2020-09-09 14:16:55 +0900

    addフォームの作成

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .forms import CreateForm
 
 
 def index(request):
     return render(request, 'todo/index.html')
+
+
+def add(request):
+    context = {
+        'form': CreateForm
+    }
+    return render(request, 'todo/add.html', context)
