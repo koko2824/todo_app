86da0ad01d1c086336811da13b01cbe21a65e849
koko2824 <prg.koko2824@gmail.com>
2020-09-11 14:17:11 +0900

    viewとurlの下準備

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import index, add
+from .views import index, add, update, delete, detail
 
 app_name = 'todo'
 
 urlpatterns = [
     path('', index.as_view(), name='index'),
     path('add/', add.as_view(), name='add'),
+    path('update/<int:pk>/', update.as_view(), name='update'),
+    path('delete/<int:pk>/', delete.as_view(), name='delete'),
+    path('detail/<int:pk>/', detail.as_view(), name='detail'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,15 +1,27 @@
-from django.views.generic import CreateView, ListView
+from django.views import generic
 from django.urls import reverse_lazy
 from .models import Todo
 from .forms import CreateForm
 
 
-class index(ListView):
+class index(generic.ListView):
     model = Todo
     template_name = 'todo/index.html'
 
 
-class add(CreateView):
+class add(generic.CreateView):
     form_class = CreateForm
     template_name = 'todo/add.html'
     success_url = reverse_lazy('todo:index')
+
+
+class update(generic.UpdateView):
+    pass
+
+
+class delete(generic.DeleteView):
+    pass
+
+
+class detail(generic.DetailView):
+    pass
